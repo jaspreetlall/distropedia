4160453d3298c2c1eba857a1abb854dfd33717cc
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-09-01 14:27:36 -0700

    Populating base and environments

--- models/Distro.js
+++ models/Distro.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Environment = require('./environment');
+const Environment = require('./Environment');
 
 const UrlSchema = new Schema({
   homepage: { type: String },
@@ -10,10 +10,10 @@ const UrlSchema = new Schema({
 const DistroSchema = new Schema({
   name: { type: String, required: true },
   descriptiion: { type: String },
-  base: [{ type: Schema.Types.ObjectId, ref: 'Distro' }],
+  baseList: [{ type: Schema.Types.ObjectId, ref: 'Distro' }],
   origin: { type: String },
   architecture: [{ type: String }],
-  environment: [{ type: Schema.Types.ObjectId, ref: "Environment" }],
+  environment: [{ type: Schema.Types.ObjectId, ref: 'Environment' }],
   status: { type: String, enum: ['active', 'inactive'] },
   visits: { type: Number },
   url: [{ UrlSchema }]
--- pages/api/distros/index.js
+++ pages/api/distros/index.js
@@ -1,21 +1,35 @@
-// const dbConnect = require('../../../lib/dbConnect');
 import dbConnect from '../../../lib/dbConnect';
 const Distro = require('../../../models/Distro');
+const Environment = require('../../../models/Environment');
 
 export default async function handler(req, res) {
   const { method } = req;
 
-  await dbConnect()
+  await dbConnect();
 
   switch (method) {
     case 'GET':
       try {
-        const distros = await Distro.find({});
+        const distros = await Distro
+        .find({})
+        .populate(
+          [
+            { path: 'environment' },
+            { path: 'baseList', select: 'name' }
+          ]
+        )
         res.status(200).json({ success: true, data: distros });
       } catch (error) {
         res.status(400).json({ success: false })
       }
-      break
+      break;
+    case 'POST':
+      try {
+        res.status(201).json({ success: true, data: "hello world" });
+      }
+      catch (error) {
+        res.status(400).json({ success: false })
+      }
     default:
       res.status(400).json({ success: false })
       break
