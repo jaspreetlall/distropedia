c88a98b1f8a1e93c660f3030c69b4be93c9c1924
Jaspreet S Lall <jaspreet.lall@gmail.com>
2021-09-27 20:00:50 -0700

    Header navigation initial setup

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,29 @@
 import styles from './Header.module.scss'
+import Link from 'next/link'
 
 function Header() {
   return (
-    <div className={styles.header}>
-      Header      
-    </div>
+    <nav className={styles.nav}>
+      <Link href="/" className={styles.nav__logo}>
+        <a className={styles.nav__logo}>
+          <h1>distropedia</h1>
+        </a>
+      </Link>
+      <div>
+        <Link href="/distros">
+          <a>/distros</a>
+        </Link>
+        <Link href="/showoff">
+          <a>/showoff</a>
+        </Link>
+        <Link href="/merch">
+          <a>/merch</a>
+        </Link>
+        <Link href="/contribute">
+          <a>/contribute</a>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
