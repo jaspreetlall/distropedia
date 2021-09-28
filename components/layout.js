f884cedab5b471be601e57113769f8f3e82534cf
Jaspreet S Lall <jaspreet.lall@gmail.com>
2021-09-27 17:22:56 -0700

    Initial setup for scss, added fonts

---
+++ components/layout.js
@@ -0,0 +1,11 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+export default function Layout({ children }) {
+  return (
+  <div>
+    <Header />
+    {children}
+    <Footer />
+  </div>)
+}
--- pages/distros/index.jsx
+++ pages/distros/index.jsx
@@ -1,36 +1,39 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router'
+import Layout from '../../components/layout';
 
 function Distros({distroList, success}) {
 
 	const router = useRouter();
 
 	return (
-		<div>
-			{
-				(success) && (distroList.length > 0)
-				? <div>
-						{
-							distroList.map((distro) => {
-								return (
-									<div key={distro._id}>
-										<h1>{distro.name}</h1>
-										<p>{distro.description}</p>
-										<Link href="/distros/[id]" as={`/distros/${distro.name}`}>
-											<a>{distro.name}</a>
-										</Link>
-									</div>
-								)
-							})
-						}
-						<button onClick={() => router.back()}>Go Back</button>
-					</div>
-				: <div>
-						<h3>We are sorry! Something went wrong!</h3>
-						<button onClick={() => router.back()}>Go Back</button>
-					</div>
-			}
-		</div>
+		<Layout>
+			<div>
+				{
+					(success) && (distroList.length > 0)
+					? <div>
+							{
+								distroList.map((distro) => {
+									return (
+										<div key={distro._id}>
+											<h1>{distro.name}</h1>
+											<p>{distro.description}</p>
+											<Link href="/distros/[id]" as={`/distros/${distro.name}`}>
+												<a>{distro.name}</a>
+											</Link>
+										</div>
+									)
+								})
+							}
+							<button onClick={() => router.back()}>Go Back</button>
+						</div>
+					: <div>
+							<h3>We are sorry! Something went wrong!</h3>
+							<button onClick={() => router.back()}>Go Back</button>
+						</div>
+				}
+			</div>
+		</Layout>
 	)
 }
 
